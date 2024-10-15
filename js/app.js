be8b448b033d9a8304fa9e91e414b30343833752
José Carlos <josecarlosmrlt@outlook.com>
2024-10-15 19:42:02 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let jogosAlugados = 0;
+
+const contaEExibeJogosAlugados = function (){
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+function alterarStatus (id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');;
+    
+    if(imagem.classList.contains("dashboard__item__img--rented")) {
+        if(confirm(`Deseja devolver ${nomeJogo.textContent}?`)){
+            imagem.classList.remove("dashboard__item__img--rented");
+            botao.classList.remove("dashboard__item__button--return");
+            botao.textContent = "Alugar";
+            jogosAlugados--;
+        }
+
+    }else {
+        imagem.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        botao.textContent = "Devolver";
+        jogosAlugados++
+    }
+    contaEExibeJogosAlugados();
+}
